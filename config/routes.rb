374dd66b577cf0c5fd0ad804c57619a058e1e166
mwaurajr <jamhurifrankiline@gmail.com>
2023-07-19 14:26:31 +0300

    generated a main page and darkomode is complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  get 'mains/index'
   root "static#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_111251) do
+  create_table "mains", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
